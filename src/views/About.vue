d75468293e68a66a87d8090914f245fa73bd45d2
avimehenwal <avi.mehanwal@gmail.com>
2020-07-28 19:09:45 +0200

    feat(component): :seedling: Able to use component from outside src
    69 Files | 344M | develop | 1 file changed, 12 insertions(+)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <TestComponent value="avi" />
   </div>
 </template>
+
+<script>
+import TestComponent from '@/../components/test.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TestComponent
+  }
+}
+</script>
