b00b95bf24e3c9c53f969efb7015e3cb81f7dcd7
avimehenwal <avi.mehanwal@gmail.com>
2020-07-31 00:46:03 +0200

    build(rollup): :dizzy: Add rollup bundler deps
    how to use built vue SFC --> add test
    
    108 Files | 826M | develop | 6 files changed, 1515 insertions(+), 31 deletions(-)

--- Apps/withjestpreset/src/components/test.vue
+++ Apps/withjestpreset/src/components/test.vue
@@ -13,6 +13,7 @@
 <script>
 
 export default {
+  name: 'TestComponent',
   props: {
     value: {
       type: String,
--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,97 @@
 import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import VuePlugin from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
 // import { nodeResolve } from '@rollup/plugin-node-resolve'; // Unresolved dependencies
+// import globals from 'rollup-plugin-node-globals';
+// import builtins from 'rollup-plugin-node-builtins';
 
-export default {
-  input: 'src/wrapper.js', // Path relative to package.json
-  output: {
-    name: 'MyComponent',
-    exports: 'named',
-    globals: {
-      'animejs/lib/anime.es.js': 'anime',
-      vue: 'vue'
+// Build more than one bundles
+export default [{
+  // Path relative to package.json
+  input: 'Apps/withjestpreset/src/components/wrapper.js',
+  output: [
+    {
+      format: 'umd',                        // umd format requires a bundles name
+      name: 'MyComponent',
+      file: 'dist/my-component.umd.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
+    },
+    {
+      format: 'es',
+      file: 'dist/my-component.esm.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
+    },
+    {
+      format: 'iife',
+      file: 'dist/my-component.min.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
     }
-  },
+  ],
+  // output: {
+  //   exports: 'named',
+  // },
   plugins: [
     commonjs(),
-    vue({
+    VuePlugin({
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true, // Explicitly convert template to render function
+      template: {
+        isProduction: true,
+      },
     }),
     buble(), // Transpile to ES5
-    // nodeResolve(),
+    // nodeResolve({ preferBuiltins: false }),  // or true
+    // globals(),
+    // builtins(),
   ],
-};
+},
+{
+  input: 'Apps/withjestpreset/src/components/temprature.vue',
+  exports: 'named',
+  globals: {
+    vue: 'VuePlugin'
+  },
+  // Build more than one bundle
+  output: [
+    {
+      format: 'umd',                        // umd format requires a bundles name
+      name: 'Temprature',
+      file: 'dist/temperature.umd.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
+    },
+    {
+      format: 'es',
+      file: 'dist/temperature.esm.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
+    },
+    {
+      format: 'iife',
+      file: 'dist/temperature.min.js',
+      globals: {
+        vue: 'VuePlugin'
+      },
+    }
+  ],
+  plugins: [
+    // vue({ template: { optimizeSSR: true } })
+    commonjs(),
+    VuePlugin({
+      css: true, // Dynamically inject css as a <style> tag
+      compileTemplate: true, // Explicitly convert template to render function
+      template: {
+        isProduction: true,
+      },
+    }),
+    buble()
+  ]
+}]
